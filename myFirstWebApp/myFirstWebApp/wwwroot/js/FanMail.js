d05c3c1a2e497981ba067b725c19f904f5f381b0
Jenna Habel <86261801+jennahabel@users.noreply.github.com>
2022-01-11 20:14:36 -0700

    second commit

---
+++ myFirstWebApp/myFirstWebApp/wwwroot/js/FanMail.js
@@ -0,0 +1,13 @@
+﻿//document.getElementById("ButtonSend").addEventListener("click", function () {
+//    alert("From: " + document.getElementById("txtFrom").value +
+//          ", Subject: " + document.getElementById("txtSub").value +
+//          ", Message: " + document.getElementById("txtMes").value);
+//})
+
+$("#ButtonSend").click(function () {
+    alert("From: " + $("#txtFrom").val() +
+        ", Subject: " + $("#txtSub").val() +
+        ", Message: " + $("#txtMes").val());
+
+    $("#pic").fadeOut("slow");
+})
